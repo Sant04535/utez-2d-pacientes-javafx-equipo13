adad7f9dbb234194c2a5379aa08d397c3e3b7a54
Santiago <20253ds099@utez.edu.mx>
2026-04-12 20:33:03 -0600

    Comentarios terminados

--- Consultorio/src/main/java/org/example/consultorio/Launcher.java
+++ Consultorio/src/main/java/org/example/consultorio/Launcher.java
@@ -1,7 +1,7 @@
 package org.example.consultorio;
 
 public class Launcher {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         HelloApplication.main(args);
     }
 }
